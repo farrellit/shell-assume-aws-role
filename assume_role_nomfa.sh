1105042f0855f6c89f93b6bbf84280c5199708a0
Dan Farrell <dfarrell@spscommerce.com>
2016-03-18 15:36:58 -0500

    Create assume_role_nomfa.sh

---
+++ assume_role_nomfa.sh
@@ -0,0 +1,21 @@
+## Assume a role without region or MFA, or Profile
+## Perfect for Jenkins jobs to achieve cross-account access
+
+## This function takes only one argument, the role name.  
+
+function assume {
+  role="$1"
+  duration=1800
+  result="$(aws sts assume-role --role-arn "$role" \
+   --role-session-name "PublicAssetsDeployment" --duration-seconds "$duration" \
+    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text \
+  )"
+  if [ $? -ne 0 ]; then 
+      echo "Failed to assume role" 1>&2; return 1; 
+  else
+    export AWS_ACCESS_KEY_ID="`echo $result | awk '{ print $1 }'`"
+    export AWS_SECRET_ACCESS_KEY="`echo $result | awk '{ print $2 }'`"
+    export AWS_SESSION_TOKEN="`echo $result | awk '{ print $3 }'`"
+    return 0
+  fi
+}

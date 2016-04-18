c95f29df1b117014ac92e1d44c8ed071adb0faee
Dan Farrell <dfarrell@spscommerce.com>
2016-04-18 16:06:00 -0500

    Update assume-update-bashrc.sh

--- assume-update-bashrc.sh
+++ assume-update-bashrc.sh
@@ -4,6 +4,8 @@ function assume {
   role=$2
   token=$3
   duration=900
+  file="$HOME/.awsrc"
+  touch $file
   if echo "$4" | grep "^[0-9]\{3,4\}$" > /dev/null; then duration=$4; fi
   mfa_device="$(aws --profile $profile --region $region iam list-mfa-devices --query MFADevices[0].SerialNumber --output text  )"
   result="$(aws --profile $profile --region $region \
@@ -17,7 +19,10 @@ function assume {
     AWS_ACCESS_KEY_ID="`echo $result | awk '{ print $1 }'`"
     AWS_SECRET_ACCESS_KEY="`echo $result | awk '{ print $2 }'`"
     AWS_SESSION_TOKEN="`echo $result | awk '{ print $3 }'`"
-    sed -i.bak -e "s/^AWS_ACCESS_KEY_ID=.*$/AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID/" -e "s/^AWS_SECRET_ACCESS_KEY=.*/AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY/" -e "s/^AWS_SESSION_TOKEN=.*$/AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN/"
+    sed -i.bak -e '/^AWS_ACCESS_KEY_ID=./d' -e '/^AWS_SECRET_ACCESS_KEY=/d' -e '/^AWS_SESSION_TOKEN=./d'  $file
+    echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"  >> $file
+    echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY/" >> $file
+    echo "export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN/" >> $file
     return 0
   fi
 }
